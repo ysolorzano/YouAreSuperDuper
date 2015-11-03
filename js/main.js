e2ac3cadce1b64c77e4884d6d720b54bd4194a8a
Yves Solorzano <solorzano.yvette@gmail.com>
2015-11-03 02:45:07 -0500

    Added some more div/change js
    ^^^

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
  $(document).ready(function() {
           $('#myContainer').multiscroll({
-            sectionsColor: ['#fff', '#FFF', '#fff', '#fff'],
-            anchors: ['first', 'second', 'third', 'fourth'],
+            sectionsColor: ['#fff', '#FFF', '#fff', '#fff', '#fff'],
+            anchors: ['first', 'second', 'third', 'fourth', 'fifth'],
             menu: '#menu',
             css3: true,
             paddingTop: '70px',
