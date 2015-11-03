59d39ce81252da425cddfdd1f8baf2336f5b5dba
Yves Solorzano <solorzano.yvette@gmail.com>
2015-11-03 13:27:22 -0500

    Clean CSS + Play with frames
    Cleaned up unnecessary CSS given with the parallax feature, also tried
    placing txt/forms/etc into iframes to clean up the html file and make
    it more modular.

--- js/main.js
+++ js/main.js
@@ -8,4 +8,16 @@
             paddingTop: '70px',
             paddingBottom: '70px'
           });
-      });
+
+          var header = document.getElementById('header');
+          var active = document.getElementsByClassName("active")
+
+          active.addEventListener('wheel', function() {
+            if (classie.has(header,"smaller")) {
+              classie.remove(header,"smaller");
+            }
+            else {
+              classie.add(header, "smaller");
+            }
+          });
+        });
