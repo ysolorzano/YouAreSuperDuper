14cb70ac55a8fb9e53978fbda1531434778ad52e
Yves Solorzano <solorzano.yvette@gmail.com>
2015-11-03 00:24:35 -0500

    Initial dump
    Layout visualized. Adding content next

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+ $(document).ready(function() {
+          $('#myContainer').multiscroll({
+            sectionsColor: ['#fff', '#FFF', '#fff', '#fff'],
+            anchors: ['first', 'second', 'third', 'fourth'],
+            menu: '#menu',
+            css3: true,
+            paddingTop: '70px',
+            paddingBottom: '70px'
+          });
+      });
